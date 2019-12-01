c0a9a385bc252d252b96d5c3d45a4d0760fb9e8c
João Mendes <joao.j.mendes@outlook.com>
2019-12-01 00:44:15 +0000

    update

--- config.js
+++ config.js
@@ -10,7 +10,10 @@ module.exports = {
     bio: "Self-taught web developer and whisky aficionado",
     contacts: {
       linkedin: "https://www.linkedin.com/in/joaojosemendes",
-      github: "https://github.com/joaojmendes",
+      github: "https://github.com/willjw3",
+      stackoverflow:
+      "https://stackoverflow.com/users/10262432/will-ward?tab=profile",
+      freecodecamp: "https://www.freecodecamp.org/willjw3",
       twitter: "https://twitter.com/joaojmendes",
     },
   },
